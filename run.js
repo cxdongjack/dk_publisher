9cc15f940d0c7578233455fb20ea042a3ca260cd
chenxiaodong <chenxiaodong@xiaomi.com>
2013-09-15 14:19:08 +0800

    add commander

--- run.js
+++ run.js
@@ -1,10 +1,13 @@
 var grunt = require('grunt');
 var path = require('path');
 
-//"dp;grunt --gruntfile publish.js --base ~/dev/duokan_store/v0/html/web/v0/develop/static/lib docore"
-var _opt = {
-  base : path.resolve('/Users/chenxiaodong/dev/duokan_store/v0/html/web/v0/develop/static/lib'),
-  gruntfile : path.resolve('publish.js')
+exports.run = function(tasks, _options) {
+  //"dp;grunt --gruntfile publish.js --base ~/dev/duokan_store/v0/html/web/v0/develop/static/lib docore"
+  var _opt = {
+    // base : path.resolve('/Users/chenxiaodong/dev/duokan_store/v0/html/web/v0/develop/static/lib'),
+    base : path.resolve(process.cwd()),
+    gruntfile : path.resolve(__dirname, 'publish.js')
+  };
+  grunt.util._.extend(_opt, _options);
+  grunt.tasks(tasks, _opt);
 };
-
-grunt.tasks(['docore'], _opt);
